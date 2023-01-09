3c130d56872037d3224e5eab6ad0023a62fb29eb
Alon Maor <48641682+AlonMaor14@users.noreply.github.com>
2023-01-09 15:40:37 +0200

    Replace `sklearn_classifier` function with `auto_trainer` (#12)

--- newflow.py
+++ newflow.py
@@ -1,3 +1,17 @@
+# Copyright 2018 Iguazio
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 from kfp import dsl
 
 from mlrun import build_function, deploy_function, get_current_project, run_function
@@ -28,13 +42,21 @@ def newpipe():
     ).after(builder)
     print(ingest.outputs)
 
+    # analyze our dataset
+    run_function(
+        "describe",
+        name="summary",
+        params={"label_column": project.get_param("label", "label")},
+        inputs={"table": ingest.outputs[DATASET]},
+    )
+
     # train with hyper-paremeters
     train = run_function(
-        "train",
+        "auto_trainer",
         name="train",
-        params={"sample": -1, "label_column": LABELS, "test_size": 0.10},
+        params={"label_columns": LABELS, "train_test_split_size": 0.10},
         hyperparams={
-            "model_pkg_class": [
+            "model_class": [
                 "sklearn.ensemble.RandomForestClassifier",
                 "sklearn.linear_model.LogisticRegression",
                 "sklearn.ensemble.AdaBoostClassifier",
@@ -48,18 +70,18 @@ def newpipe():
 
     # test and visualize our model
     run_function(
-        "test",
+        "auto_trainer",
         name="test",
-        params={"label_column": LABELS},
+        handler="evaluate",
+        params={"label_columns": LABELS, "model": train.outputs["model"]},
         inputs={
-            "models_path": train.outputs["model"],
-            "test_set": train.outputs["test_set"],
+            "dataset": train.outputs["test_set"],
         },
     )
 
     # deploy our model as a serverless function, we can pass a list of models to serve
     deploy = deploy_function(
-        "serve",
+        "serving",
         models=[{"key": f"{DATASET}:v1", "model_path": train.outputs["model"]}],
     )
 
